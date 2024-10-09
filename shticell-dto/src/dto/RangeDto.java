4a0fc32de93f265a294db6f06fabff5866dc9895
raviv5 <itayraviv5@gmail.com>
2024-10-09 14:39:26 +0300

    convert to dto sucsseded.

--- shticell-dto/src/dto/RangeDto.java
+++ shticell-dto/src/dto/RangeDto.java
@@ -1,8 +1,14 @@
 package dto;
 
+import sheet.range.api.RangeGetters;
 import sheet.range.boundaries.impl.BoundariesImpl;
 
 public class RangeDto {
     public String name;
     public BoundariesImpl bounds;
+
+    public RangeDto(RangeGetters range) {
+        this.name = range.getName();
+        this.bounds = (BoundariesImpl) range.getBoundaries();
+    }
 }
--- shticell-dto/src/dto/SheetDto.java
+++ shticell-dto/src/dto/SheetDto.java
@@ -1,10 +1,12 @@
 package dto;
 
-import sheet.cell.api.Cell;
+import engine.api.Engine;
+import engine.impl.EngineImpl;
+import sheet.api.SheetGetters;
 import sheet.coordinate.api.Coordinate;
-import sheet.layout.api.Layout;
-import sheet.range.api.Range;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,4 +16,36 @@ public class SheetDto {
     public int version;
     public Map<Coordinate, CellDto> activeCells;
     public Set<RangeDto> ranges;
+
+
+
+    public SheetDto(SheetGetters sheet) {
+        this.name = sheet.getName();
+        this.version = sheet.getVersion();
+        this.layout = new LayoutDto(sheet.getLayout());
+        this.activeCells = new HashMap<>();
+        this.ranges = new HashSet<>();
+        sheet.getActiveCells().forEach((coordinate, cell) -> activeCells.put(coordinate,new CellDto(cell)));
+        activeCells.values().forEach(CellDto::setInfluenceOn);
+        sheet.getRanges().forEach(range-> ranges.add(new RangeDto(range)));
+    }
+
+    public static void main(String[] args) {
+        //test
+        Engine  e = EngineImpl.create();
+        String file = "C:/Users/itayr/OneDrive/Desktop/second year/java/shticell_client_server/Engine/src/engine/jaxb/resources/grades.xml";
+        e.readXMLInitFile(file);
+        SheetDto dto = new SheetDto(e.getSheetStatus());
+        System.out.println(dto.name);
+        System.out.println(dto.layout.rows);
+        System.out.println(dto.layout.columns);
+
+        dto.activeCells.forEach((coordinate, cell) -> {
+            System.out.println("" + coordinate +" ="+ cell.effectiveValue+"");
+            cell.influenceFrom.forEach(cellDto -> System.out.println(cellDto.coordinate));
+            cell.influenceOn.forEach(cellDto -> System.out.println(cellDto.coordinate));
+        });
+        dto.ranges.forEach(range -> System.out.println(range.name));
+
+    }
 }
