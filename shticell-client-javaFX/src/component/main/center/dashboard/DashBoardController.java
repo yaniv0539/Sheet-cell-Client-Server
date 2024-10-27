e3b2ff6af4b49eda812c465b49e90ec0e0c24e57
Yaniv Zamir <yanivza@mta.ac.il>
2024-10-27 14:08:34 +0200

    chat added

--- shticell-client-javaFX/src/component/main/center/dashboard/DashBoardController.java
+++ shticell-client-javaFX/src/component/main/center/dashboard/DashBoardController.java
@@ -218,7 +218,7 @@ public class DashBoardController {
     @FXML
     void initialize() {
         initPullThread();
-//        initBindButtonDisableProperty();
+        initBindButtonDisableProperty();
         initSheetTableView();
         initRequestTableView();
         initListeners();
